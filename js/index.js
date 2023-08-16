f4fd01e19e72bf180474be96303bcfed698c0d81
Veronica_Olivera <ve.olivera@hotmail.com>
2023-08-16 18:29:38 -0300

    cambio de Logueate a Inicia Sesión

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
+
+// Redirige el index al login en caso de no estar logeado
+var loggedin = localStorage.getItem("email")
+if (!loggedin){
+    window.location = "login.html"
+}
